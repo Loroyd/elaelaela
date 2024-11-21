f96e797ff1e5281ceaeb77b34173688c28b35f5e
Lloyd <johnandres1450@gmail.com>
2024-11-21 15:02:37 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,104 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+navLinks.forEach((link) => {
+  link.onclick = () => {
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+  };
+});
+(function () {
+  "use strict";
+  const forms = document.querySelectorAll(".needs-validation");
+  const result = document.getElementById("result");
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.querySelectorAll(":invalid")[0].focus();
+        } else {
+          const formData = new FormData(form);
+          event.preventDefault();
+          event.stopPropagation();
+          const object = {};
+          formData.forEach((value, key) => {
+            object[key] = value;
+          });
+          const json = JSON.stringify(object);
+          result.innerHTML = "Please wait...";
+          fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: json,
+          })
+            .then(async (response) => {
+              let json = await response.json();
+              if (response.status == 200) {
+                Swal.fire({
+                  title: "Success!",
+                  text: "Message sent successfully!",
+                  icon: "success",
+                });
+                result.innerHTML = "";
+                result.classList.remove("text-red-500");
+                result.classList.add("text-green-500");
+              } else {
+                console.log(response);
+                result.innerHTML = json.message;
+                result.classList.remove("text-green-500");
+                result.classList.add("text-red-500");
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+              result.innerHTML = "Something went wrong!";
+              Swal.fire({
+                title: "Error!",
+                text: "Failed to send the message.",
+                icon: "error",
+              });
+            })
+            .then(function () {
+              form.reset();
+              form.classList.remove("was-validated");
+              setTimeout(() => {
+                result.style.display = "none";
+              }, 5000);
+            });
+        }
+        form.classList.add("was-validated");
+      },
+      !1
+    );
+  });
+})();
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
